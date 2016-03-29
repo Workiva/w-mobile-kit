05d012d17f6a5bf10e1ab36c2cb7fa587c13348a
James Romo <james.romo@workiva.com>
2016-03-29 12:38:13 -0700

    MOBILE-1625: Setup files for consumption

---
+++ Source/WMobileKit.h
@@ -0,0 +1,19 @@
+//
+//  WMobileKit.h
+//  WMobileKit
+//
+//  Created by James Romo on 3/29/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WMobileKit.
+FOUNDATION_EXPORT double WMobileKitVersionNumber;
+
+//! Project version string for WMobileKit.
+FOUNDATION_EXPORT const unsigned char WMobileKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WMobileKit/PublicHeader.h>
+
+
