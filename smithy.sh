68d45f0346993a709858b776d004667b51520278
James Romo <james.romo@workiva.com>
2016-04-14 09:14:43 -0700

    MOBILE-1659: Add gem installs and run unit tests

--- smithy.sh
+++ smithy.sh
@@ -1,4 +1,22 @@
 #!/usr/bin/env bash
 source ./core.sh
 
-echo "Smithy script has not be set up"
+# Make sure we're using the rbenv version of ruby
+PATH="$(cd ~/; pwd)/.rbenv/shims:$(cd ~/; pwd)/.rbenv/bin:$PATH"
+
+function run_unit_tests() {
+    echo "Starting unit tests."
+
+    xcodebuild -workspace WMobileKit.xcworkspace -scheme WMobileKit -enableCodeCoverage YES -sdk iphonesimulator test | ocunit2junit
+
+    unit_test_failure_check
+}
+
+function unit_test_failure_check {
+    # Due to piping the results into ocunit2junit, $? does not represent testing results
+    if [ ${PIPESTATUS[0]} -ne 0 ]; then
+        print_error "ERROR: iOS unit test failure(s). See above for details."
+    fi
+}
+
+run_unit_tests
