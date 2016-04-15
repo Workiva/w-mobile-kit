63779144ead175fa45ccae1a917ae2c8e7ba810f
James Romo <james.romo@workiva.com>
2016-04-15 09:08:15 -0700

    MOBILE-1659: Remove code coverage

--- smithy.sh
+++ smithy.sh
@@ -7,8 +7,11 @@ PATH="$(cd ~/; pwd)/.rbenv/shims:$(cd ~/; pwd)/.rbenv/bin:$PATH"
 function run_unit_tests() {
     echo "Starting $1 unit tests."
 
+    #xcodebuild clean test -workspace WMobileKit.xcworkspace -scheme WMobileKit -configuration Debug -destination \
+    #"platform=iOS Simulator,name=$1,OS=8.4" -enableCodeCoverage YES | ocunit2junit
+
     xcodebuild clean test -workspace WMobileKit.xcworkspace -scheme WMobileKit -configuration Debug -destination \
-    "platform=iOS Simulator,name=$1,OS=8.4" -enableCodeCoverage YES | ocunit2junit
+    "platform=iOS Simulator,name=$1,OS=8.4" | ocunit2junit
 
     unit_test_failure_check
 }
@@ -41,17 +44,17 @@ killall Xcode
 clean_previous_build
 echo "Starting iPad Simulator."
 open -a Simulator --args -CurrentDeviceUDID 7BE2512A-6B44-4FFF-96A8-60BF0D7269A1; sleep 3
-run_unit_tests2 "iPad Retina"
+run_unit_tests "iPad Retina"
 
 # Running unit tests, we need to open the simulator to make sure xcodebuild knows that it is open.
 clean_previous_build
 echo "Starting iPhone Simulator."
 open -a Simulator --args -CurrentDeviceUDID 5E5091B3-63F2-4C60-8FF8-E30BBEC8383B; sleep 3
-run_unit_tests2 "iPhone 5"
+run_unit_tests "iPhone 5"
 
-echo "Generating code coverae report"
-xcov -w WMobileKit.xcworkspace -s WMobileKit -o xcov
-mkdir code_coverage
-zip -r -X code_coverage/coverage.zip xcov
+#echo "Generating code coverage report"
+#xcov -w WMobileKit.xcworkspace -s WMobileKit -o xcov
+#mkdir code_coverage
+#zip -r -X code_coverage/coverage.zip xcov
 
 echo "Exiting with status: $?"
