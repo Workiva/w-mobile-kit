ab75c84b7099e52ef003fc736912a8245c4e8c20
jeffscaturro-wf <jeff.scaturro@workiva.com>
2016-05-10 10:41:01 -0700

    MOBILE-1671: Cleaning up custom slide animation.

--- Source/WCustomTransitions.swift
+++ Source/WCustomTransitions.swift
@@ -39,44 +39,24 @@ public class SlideAnimationController: NSObject, UIViewControllerAnimatedTransit
         let offScreenRight = CGAffineTransformMakeTranslation(container.frame.width, 0)
         let offScreenLeft = CGAffineTransformMakeTranslation(-container.frame.width, 0)
 
-        // prepare the toView for the animation
         toView.transform = self.presenting ? offScreenRight : offScreenLeft
 
-        // set the anchor point so that rotations happen from the top-left corner
-        toView.layer.anchorPoint = CGPoint(x:0, y:0)
-        fromView.layer.anchorPoint = CGPoint(x:0, y:0)
-
-        // updating the anchor point also moves the position to we have to move the center position to the top-left to compensate
-        toView.layer.position = CGPoint(x:0, y:0)
-        fromView.layer.position = CGPoint(x:0, y:0)
-
-        // add the both views to our view controller
         container.addSubview(toView)
         container.addSubview(fromView)
 
-        // get the duration of the animation
-        // DON'T just type '0.5s' -- the reason why won't make sense until the next post
-        // but for now it's important to just follow this approach
         let duration = self.transitionDuration(transitionContext)
 
-        // perform the animation!
-        // for this example, just slid both fromView and toView to the left at the same time
-        // meaning fromView is pushed off the screen and toView slides into view
-        // we also use the block animation usingSpringWithDamping for a little bounce
         UIView.animateWithDuration(duration,
             delay: 0.0,
             usingSpringWithDamping: 1,
             initialSpringVelocity: 1,
             options: .CurveEaseInOut,
             animations: {
-                // slide fromView off either the left or right edge of the screen
-                // depending if we're presenting or dismissing this view
                 fromView.transform = self.presenting ? offScreenLeft : offScreenRight
                 toView.transform = CGAffineTransformIdentity
 
                 },
             completion: { finished in
-                // tell our transitionContext object that we've finished animating
                 transitionContext.completeTransition(true)
             })
     }
