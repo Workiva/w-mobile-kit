4404182d5a77afb21a2438d9119da40264021dbf
James Romo <james.romo@workiva.com>
2016-09-13 09:20:58 -0700

    MOBILE-2160: Pinned down podspec libraries. Bumped and pinned down testing libraries. Updated version bump script to update pods.

--- version_bump.sh
+++ version_bump.sh
@@ -8,13 +8,29 @@ function usage {
     echo "    ./version_bump.sh 2.6.0 2.6.1"
 }
 
-if [[ "$#" -lt 1 ]] ; then
-    usage
-else
+# Usage: updateVersion $arg1 $arg2
+function updateVersion {
+    echo
+    echo "Updating version from $1 to $2"
+    echo
     # Spaces are necessary
     sed -i "" -E "s/s.version          = '$1'/s.version          = '$2'/g" WMobileKit.podspec
 
     sed -i "" -E "s/<string>$1<\/string>/<string>$2<\/string>/g" Source/Info.plist
 
     sed -i "" -E "s/<string>$1<\/string>/<string>$2<\/string>/g" Example/WMobileKitExample/Info.plist
+}
+
+function updatePods {
+    echo
+    echo "Updating Pods for new version"
+    echo
+    ./setup.sh
+}
+
+if [[ "$#" -lt 1 ]] ; then
+    usage
+else
+    updateVersion $1 $2
+    updatePods
 fi
